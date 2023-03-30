4646add6531d434c7442c8aeb21729395ff058b1
Matheus Torres <matheustorres@poli.ufrj.br>
2023-03-30 14:18:52 -0300

    feat: initial Node project

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+const UserController = require("./controllers/UserController");
+
+module.exports = [
+  {
+    method: "GET",
+    endpoint: "/users",
+    handler: UserController.listUsers,
+  },
+  {
+    method: "GET",
+    endpoint: "/users/:id",
+    handler: UserController.getUserById,
+  },
+];
